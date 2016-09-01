89bbc00c7f77693cad1514f48121246a28fcd98b
MitchellPThayer <mitchellthayer@gmail.com>
2016-09-01 18:38:56 -0500

    New quickcode: BatchSave.m

---
+++ BatchSave.m
@@ -0,0 +1,46 @@
+% BatchSave.m: MPT 2016.08.30 
+% Saves a bunch of figure handles as FIG & PNG
+% Nothing particularly new & exciting, just saving time for IGACplots2016.m
+%
+%%%%%%%%%
+% INPUTS:
+% FigureHandles -- Array of figure handles
+% strFigureName -- Base name for saving files ('Test_1', 'Test_2',...)
+% strOutputPath -- Where to save files
+%
+% OPTIONAL INPUTS:
+% PositionArray -- 1x4 Position Array
+% qCloseFigs -- Close figures once saved? [0,1] default: 0
+
+function [] = BatchSave(FigureHandles, strFigureName, strOutputPath, varargin)
+%% I/O
+% Deal with inputs:
+% (specify defaults and valid options)
+varInfo = {...
+    %name               %default            %valid
+    'PositionArray',     [],                [];...
+    'qCloseFigs',        0,                 [0,1];...
+    };
+ParsePairs(varargin,varInfo);
+
+for i = 1:length(FigureHandles);
+    thisF = FigureHandles(i);
+    
+    prevDockedness = thisF.WindowStyle; % Was this docked or normal?
+    
+    if ~isempty(PositionArray) % if desired, resize
+        thisF.WindowStyle = 'normal'; % to resize, must ensure undocked
+        set(thisF,'Position',PositionArray);
+    end
+
+    DoubleSave(thisF, [strFigureName,'_',num2str(i)], strOutputPath);
+
+    if qCloseFigs % if desired, close the figure
+        close(thisF)        
+    else
+        thisF.WindowStyle = prevDockedness; % make sure it is left as is (was).
+    end
+    
+
+end
+end
